65c70fa160125898acc94baaf73d104a639e9800
vienamaulina <vienamaulina19@gmail.com>
2021-11-04 06:55:20 +0700

    program luas dan keliling lingkaran

---
+++ latihan3.py
@@ -0,0 +1,8 @@
+print("Menghitung Luas dan Keliling Lingkaran")
+
+pi = 22/7
+r=int(input("masukan jari jari : "))
+print("Luas lingkaran adalah = ",pi*r*r,"cm")
+print("Keliling lingkaran adalah = ",2*pi*r,"cm")
+
+
